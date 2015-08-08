f624339a1e02dd50b9a6c0c9c4199f33286eed4d
yyx290799684 <yyx290799684@hotmail.com>
2015-08-08 13:46:11 +0800

    成绩查询

--- ZSCY/Pages/ScorePage.xaml.cs
+++ ZSCY/Pages/ScorePage.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Phone.UI.Input;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ZSCY.Data;
+using ZSCY.Util;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
@@ -23,8 +28,10 @@ namespace ZSCY.Pages
     /// </summary>
     public sealed partial class ScorePage : Page
     {
+        private ApplicationDataContainer appSetting;
         public ScorePage()
         {
+            appSetting = ApplicationData.Current.LocalSettings; //本地存储
             this.InitializeComponent();
         }
 
@@ -36,6 +43,7 @@ namespace ZSCY.Pages
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             HardwareButtons.BackPressed += HardwareButtons_BackPressed;//注册重写后退按钮事件
+            initScore();
         }
 
         private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)//重写后退按钮，如果要对所有页面使用，可以放在App.Xaml.cs的APP初始化函数中重写。
@@ -53,6 +61,31 @@ namespace ZSCY.Pages
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             HardwareButtons.BackPressed -= HardwareButtons_BackPressed;//注册重写后退按钮事件
+        }
+
+        private async void initScore()
+        {
+            List<KeyValuePair<String, String>> paramList = new List<KeyValuePair<String, String>>();
+            paramList.Add(new KeyValuePair<string, string>("stuNum", appSetting.Values["stuNum"].ToString()));
+            paramList.Add(new KeyValuePair<string, string>("idNum", appSetting.Values["idNum"].ToString()));
+            string score = await NetWork.getHttpWebRequest("api/examGrade", paramList);
+            Debug.WriteLine("score->" + score);
+            if (score != "")
+            {
+                JObject obj = JObject.Parse(score);
+                if (Int32.Parse(obj["status"].ToString()) == 200)
+                {
+                    List<ScoreList> scoreList = new List<ScoreList>();
+                    JArray ScoreListArray = Utils.ReadJso(score);
+                    for (int i = 0; i < ScoreListArray.Count; i++)
+                    {
+                        ScoreList classitem = new ScoreList();
+                        classitem.GetAttribute((JObject)ScoreListArray[i]);
+                        scoreList.Add(classitem);
+                    }
+                    ScoreListView.ItemsSource = scoreList;
+                }
+            }
         }
     }
 }
