9870323aca92ed6dc1a9439a419da50fd2b307c2
yyx290799684 <yyx290799684@hotmail.com>
2015-08-16 20:34:41 +0800

    重置

--- ZSCY/Pages/SettingPage.xaml.cs
+++ ZSCY/Pages/SettingPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -7,6 +8,7 @@ using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Phone.UI.Input;
+using Windows.Storage;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -15,6 +17,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ZSCY.Util;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
@@ -25,8 +28,10 @@ namespace ZSCY.Pages
     /// </summary>
     public sealed partial class SettingPage : Page
     {
+        private ApplicationDataContainer appSetting;
         public SettingPage()
         {
+            appSetting = ApplicationData.Current.LocalSettings; //本地存储
             this.InitializeComponent();
         }
 
@@ -68,7 +73,7 @@ namespace ZSCY.Pages
             var result = await dig.ShowAsync();
             if (null != result && result.Label == "是")
             {
-            Frame.Navigate(typeof(ImportKB2CalendarPage));
+                Frame.Navigate(typeof(ImportKB2CalendarPage));
             }
             else if (null != result && result.Label == "否")
             {
@@ -78,6 +83,34 @@ namespace ZSCY.Pages
         private void AboutAppBarToggleButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(AboutPage));
+        }
+
+        private async void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dig = new MessageDialog("若应用无法使用，请尝试清除数据，清除数据后会自动退出应用。\n\n是否继续？", "警告");
+            var btnOk = new UICommand("是");
+            dig.Commands.Add(btnOk);
+            var btnCancel = new UICommand("否");
+            dig.Commands.Add(btnCancel);
+            var result = await dig.ShowAsync();
+            if (null != result && result.Label == "是")
+            {
+                appSetting.Values.Clear();
+                IStorageFolder applicationFolder = ApplicationData.Current.LocalFolder;
+                IStorageFile storageFileWR = await applicationFolder.CreateFileAsync("kb", CreationCollisionOption.OpenIfExists);
+                try
+                {
+                    storageFileWR.DeleteAsync();
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("设置 -> 重置应用异常");
+                }
+                Application.Current.Exit();
+            }
+            else if (null != result && result.Label == "否")
+            {
+            }
         }
     }
 }
