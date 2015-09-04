3e4fd0f27bc32ae4681a35f3e5695c7a3a1ada94
yyx290799684 <yyx290799684@hotmail.com>
2015-09-04 14:05:32 +0800

    缓存时候的周次。。没做好

--- ZSCY_Win10/Util/Utils.cs
+++ ZSCY_Win10/Util/Utils.cs
@@ -46,6 +46,26 @@ namespace ZSCY_Win10.Util
         }
 
         /// <summary>
+        /// 获取当前日期是今年的第几周
+        /// </summary>
+        /// <returns></returns>
+        public static int GetWeekOfYear()
+        {
+            //一.找到第一周的最后一天（先获取1月1日是星期几，从而得知第一周周末是几）
+            int firstWeekend = 7 - Convert.ToInt32(DateTime.Parse(DateTime.Today.Year + "-1-1").DayOfWeek);
+
+            //二.获取今天是一年当中的第几天
+            int currentDay = DateTime.Today.DayOfYear;
+
+            //三.（今天 减去 第一周周末）/7 等于 距第一周有多少周 再加上第一周的1 就是今天是今年的第几周了
+            //    刚好考虑了惟一的特殊情况就是，今天刚好在第一周内，那么距第一周就是0 再加上第一周的1 最后还是1
+            return Convert.ToInt32(Math.Ceiling((currentDay - firstWeekend) / 7.0)) + 1;
+        }
+
+
+
+
+        /// <summary>
         ///UNICODE字符转为中文 
         /// </summary>
         /// <param name="unicodeString"></param>
